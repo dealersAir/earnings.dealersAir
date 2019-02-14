ac7b855e850a92f66b944ea600bda5069db3bd60
Lufter <lufter21@gmail.com>
2019-02-14 14:49:34 +0200

    first commit

---
+++ dist/templates/home.php
@@ -0,0 +1,43 @@
+<?php
+$meta = array('title' => 'Инфокурсы');
+include $_SERVER['DOCUMENT_ROOT'] .'/templates/inc/header.php';
+?>
+
+<!--MAIN/-->
+<main class="main">
+	<div id="top-slider" class="top-slider">
+		<div class="top-slider__item">
+
+		</div>
+	</div>
+   <div class="section">
+      <div class="row row_wrp">
+         <div class="col-12">
+            <h1 class="title">Инфокурсы</h1>
+         </div>
+      </div>
+      <div class="row row_wrp">
+         <div class="col-12 p-0">
+            <div class="post row">
+               <div class="col-4">
+                  <div class="post__thumb">
+                     <img src="/static/images/img1.jpg" alt="img">
+                  </div>
+               </div>
+               <div class="col-8">
+                  <h2 class="post__tit">Lorem ipsum dolor sit amet</h2>
+                  <p class="post__exc">
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam esse enim odio eaque animi reiciendis distinctio est quos, perferendis fugiat earum rerum nam exercitationem, quas, inventore voluptas qui consequatur adipisci?
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+      
+   </div>
+</main>
+<!--/MAIN-->
+
+<?php
+include $_SERVER['DOCUMENT_ROOT'] .'/templates/inc/footer.php';
+?>
